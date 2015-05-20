4a237a32bfae4ddbd7967ef0fbc69351e42ae1fb
jselabzxl <15195817207@163.com>
2015-05-20 09:42:00 +0800

    Update build_scrapy_guba_redis_image.sh

--- build_scrapy_guba_redis_image.sh
+++ build_scrapy_guba_redis_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -r scrapy_guba_redis
-git clone https://github.com/linhaobuaa/scrapy_guba_redis.git
+git clone https://github.com/jselabzxl/scrapy_guba_redis.git
 docker build -t scrapy_guba_redis:0.1.0 .
